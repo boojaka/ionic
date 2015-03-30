6b21721ca7c2a24076aa6264b8c27d356806be6e
boojaka <boojaka@gmail.com>
2015-03-30 20:52:54 +0300

    pounch db - sync ready

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -40,7 +40,49 @@ angular.module('starter.controllers', [])
     };
   })
 
-  .controller('ChatsCtrl', function($scope, todo) {
+  .controller('ChatsCtrl', function(todoDb,$scope, todo) {
+    $scope.online=false;
+
+    $scope.toggle=function(){
+      $scope.online=!$scope.online;
+      if ($scope.online) {  // Read http://pouchdb.com/api.html#sync
+        $scope.sync = todoDb.sync('http://127.0.0.1:5984/todos', {live: true})
+          .on('error', function (err) {
+            console.log("Syncing stopped");
+            console.log(err);
+          });
+      } else {
+        $scope.sync.cancel();
+      }
+    };
+    todoDb.changes({
+      live: true,
+      onChange: function (change) {
+        if (!change.deleted) {
+          todoDb.get(change.id, function(err, doc) {
+            if (err) console.log(err);
+            $scope.$apply(function() { //UPDATE
+              for (var i = 0; i < $scope.tasks.length; i++) {
+                if ($scope.tasks[i]._id === doc._id) {
+                  $scope.tasks[i] = doc;
+                  return;
+                }
+              } // CREATE / READ
+              $scope.tasks.push(doc);
+            });
+          })
+        } else { //DELETE
+          $scope.$apply(function () {
+            for (var i = 0; i<$scope.tasks.length; i++) {
+              if ($scope.tasks[i]._id === change.id) {
+                $scope.tasks.splice(i,1);
+              }
+            }
+          })
+        }
+      }
+    });
+
     $scope.items=todo.all();
     $scope.data={
       text:''
@@ -63,4 +105,8 @@ angular.module('starter.controllers', [])
       auth.logout();
       $state.go('login');
     };
-  });
+  })
+  .factory('todoDb', function() {
+    var db = new PouchDB('todos');
+    return db;
+  })
